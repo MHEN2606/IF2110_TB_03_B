aa4bf11866b12ee66c604de3a5b95d9573dd8291
varrazha <106954599+varrazha@users.noreply.github.com>
2022-11-19 13:39:47 +0700

    redo 20 21 at my despair, sorry all

--- src/liststatik/dliststatik.c
+++ src/liststatik/dliststatik.c
@@ -17,38 +17,43 @@ gcc dliststatik.c charmachine.o wordmachine.o time.o set.o liststatik.o tree.o f
 int main()
 {
     ListStatik fd,resep,copymakanan,child,notif,map,mapresep;
-    Word temp;
+    Word temp, temp1;
     Set S;
     /*
     Untuk menampilkan food harus mengubah path di liststatik menjadi
     ../../config/makanan.txt
     ../../config/resep.txt
     */
+    printf("================================\n");
+    printf("||******DRIVER LISTSTATIK*****||\n");
+    printf("================================\n");
     inputListFood(&fd);
     displayList(fd);
     readResep(&resep);
-    displayResep(resep);
-    printf("Panjang List Statik Resep\n");
-    printf("%d",panjangList(resep));
-    printf("Copy makanan ke list copymakanan\n");
+    printf("\nPanjang List Statik Resep : ");
+    printf("%d\n",panjangList(resep));
+    printf("\nCopy makanan ke list copymakanan\n");
     copymakanan = copyList(&fd);
     displayList(copymakanan);
-    printf("Makanan dengan id 11\n");
+    printf("\nMakanan dengan id 11\n");
     temp = findFdName(11,fd);
     tulisKata(temp);
-    printf("Aksi dari makana dengan id 11\n");
-    temp = findFdAksi(11,fd);
-    printf("Waktu exp untuk makanan dengan id 11\n");
-    printf("%d",findFdExp(11,fd));
+    printf("\nAksi dari makanan dengan id 11\n");
+    temp1 = findFdAksi(11,fd);
+    tulisKata(temp1);
+    printf("\nWaktu exp untuk makanan dengan id 11\n");
+    printf("%d detik\n",findFdExp(11,fd));
     printf("Waktu delivery untuk makanan dengan id 11\n");
-    printf("%d",findFdDel(11,fd));
+    printf("%d detik\n",findFdDel(11,fd));
     printf("BUKU RESEP\n");
     bukuResep(resep,fd);
     child = findChild(37,resep);
+    printf("Isi child dari id 37\n");
     for (int i = 0;i <NEFF(child);i++)
     {
         printf("|%d|",IINFO(child,i));
     }
+    printf("\n");
     createNotificationList(&notif);
     Word ElNotif = {"FRY",3};
     Word ElNotif2 = {"AYAM",4};
@@ -57,10 +62,15 @@ int main()
     NAMANOTIF(tempNotif) = ElNotif2;
     insertNotif(&notif,tempNotif);
     displayNotif(&notif);
+    printf("\n");
+    printf("Buat Map dan isi Dengan Set\n");
     createMap(&map);
     createSet(&S);
     insert(&S,11);
     insert(&S,12);
     insertElMap(&map,S,11);
     makeMapOfResep(resep,&mapresep);
+    printf("KEY : %d\n",KEY(MINFO(map,0)));
+    printf("SET : ");
+    displaySet(SET(MINFO(map,0)));
 }
